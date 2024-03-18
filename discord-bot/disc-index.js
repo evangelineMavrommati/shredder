9e7db115f23b31ec9fe43a7488fde04dadd7112a
evangelineMavrommati <emavro19@gmail.com>
2024-03-18 18:05:26 -0500

    Convert js to ts
    Discord.js has a hard node.js dependency so I will not be able to use
    bun as the runtime. I don't think just installing typescript will fully
    resolve the issue either.
    
    Exploring refactoring options around this.

--- discord-bot/disc-index.js
+++ discord-bot/disc-index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
-// const fs = require('node:fs');
-// const path = require('node:path');
+const { Client, Events, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
 const token = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
@@ -11,8 +11,8 @@ client.commands = new Collection();
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient: { user: { tag: any; }; }) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 // Log in to Discord with your client's token
